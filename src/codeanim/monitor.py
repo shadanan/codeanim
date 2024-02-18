710b125b130385167d4408ed86f121acc14c3b91
Shad Sharma <shadanan@gmail.com>
2024-02-17 17:15:25 -0800

    Improve public interface for key monitor and delays

--- src/codeanim/monitor.py
+++ src/codeanim/monitor.py
@@ -7,28 +7,34 @@ class KeyMonitor:
     def __init__(self):
         self.press = Event()
         self.released: Key | KeyCode | None = None
+        self.listener: Listener | None = None
 
     def handle_release(self, key: Key | KeyCode | None):
         self.released = key
         self.press.set()
 
+    def __enter__(self):
+        self.start()
+        return self
+
     def start(self):
         self.listener = Listener(on_release=self.handle_release)
         self.listener.start()
+        return self
+
+    def __exit__(self, *args):
+        self.stop()
 
     def stop(self):
-        self.listener.stop()
+        if self.listener is not None:
+            self.listener.stop()
+            self.listener.join()
+            self.listener = None
 
-    def wait(self, key: Key):
+    def wait(self, key: Key = Key.shift):
+        if self.listener is None:
+            raise RuntimeError("KeyMonitor is not running.")
         print(f"Press and release {key.name} to continue")
         while key != self.released:
             self.press.wait()
             self.press.clear()
-
-
-monitor = KeyMonitor()
-monitor.start()
-
-
-def wait(key: Key = Key.shift):
-    monitor.wait(key)
